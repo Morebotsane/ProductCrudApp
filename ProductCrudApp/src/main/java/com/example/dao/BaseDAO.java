636acf7743135e0cf98ebcdae458f38f9ca42907
Morebotsane <morebotsane@gmail.com>
2025-08-18 08:14:01 +0100

    On Saturday morning we were working on pagination for the product service. We struggled a bit and ended up making changes to the BaseDAO. We are going to work on pagination for the rest of the resources in our application and then end up refactoring the BaseDAO so that we can abstract all the similar code.

--- ProductCrudApp/src/main/java/com/example/dao/BaseDAO.java
+++ ProductCrudApp/src/main/java/com/example/dao/BaseDAO.java
@@ -4,31 +4,36 @@ import java.util.List;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+/**
+ * BaseDAO provides generic CRUD operations for entities.
+ * Safe for container-managed EntityManagers.
+ */
 public abstract class BaseDAO<T> {
 
     @PersistenceContext(unitName = "ProductPU")
-    protected EntityManager em;
-    
-    public EntityManager getEntityManager() {
-        return em;
-    }
-    
-    public T findById(Class<T> entityClass, Object id) {
-        return em.find(entityClass, id);
-    }
-    
-    public T findById(Object id) {
-        return find(id);
-    }
-    
-    public void save(T entity) {
-        em.persist(entity);
-    }
+    protected EntityManager em; // container-managed
 
     private final Class<T> entityClass;
 
     protected BaseDAO(Class<T> entityClass) {
         this.entityClass = entityClass;
+    }
+
+    /** Provide access to the container-managed EntityManager */
+    public EntityManager getEntityManager() {
+        return em;
+    }
+
+    public T findById(Class<T> clazz, Object id) {
+        return em.find(clazz, id);
+    }
+
+    public T findById(Object id) {
+        return em.find(entityClass, id);
+    }
+
+    public void save(T entity) {
+        em.persist(entity);
     }
 
     public void create(T entity) {
@@ -38,9 +43,14 @@ public abstract class BaseDAO<T> {
     public T find(Object id) {
         return em.find(entityClass, id);
     }
-    
+
     public List<T> findAll(Class<T> clazz) {
         return em.createQuery("SELECT e FROM " + clazz.getSimpleName() + " e", clazz)
+                 .getResultList();
+    }
+
+    public List<T> findAll() {
+        return em.createQuery("SELECT e FROM " + entityClass.getSimpleName() + " e", entityClass)
                  .getResultList();
     }
 
@@ -51,10 +61,6 @@ public abstract class BaseDAO<T> {
     public void delete(T entity) {
         em.remove(em.contains(entity) ? entity : em.merge(entity));
     }
-
-    public List<T> findAll() {
-        return em.createQuery("SELECT e FROM " + entityClass.getSimpleName() + " e", entityClass)
-                 .getResultList();
-    }
 }
+
 
--- ProductCrudApp/src/main/java/com/example/dao/ProductDAO.java
+++ ProductCrudApp/src/main/java/com/example/dao/ProductDAO.java
@@ -1,56 +1,53 @@
 package com.example.dao;
 
 import com.example.entities.Product;
-import jakarta.persistence.EntityManager;
+import jakarta.ejb.Stateless;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
 
+@Stateless
 public class ProductDAO extends BaseDAO<Product> {
 
     public ProductDAO() {
         super(Product.class);
     }
 
+    /**
+     * Retrieve paginated and optionally filtered products.
+     */
     public List<Product> findProducts(int offset, int limit, String nameFilter) {
-        EntityManager em = getEntityManager();
-        try {
-            String jpql = "SELECT p FROM Product p";
-            if (nameFilter != null && !nameFilter.isEmpty()) {
-                jpql += " WHERE LOWER(p.name) LIKE LOWER(:name)";
-            }
-
-            TypedQuery<Product> query = em.createQuery(jpql, Product.class);
-
-            if (nameFilter != null && !nameFilter.isEmpty()) {
-                query.setParameter("name", "%" + nameFilter + "%");
-            }
-
-            query.setFirstResult(offset);
-            query.setMaxResults(limit);
-
-            return query.getResultList();
-        } finally {
-            em.close();
+        String jpql = "SELECT p FROM Product p";
+        if (nameFilter != null && !nameFilter.isEmpty()) {
+            jpql += " WHERE LOWER(p.name) LIKE LOWER(:name)";
         }
+
+        TypedQuery<Product> query = getEntityManager().createQuery(jpql, Product.class);
+
+        if (nameFilter != null && !nameFilter.isEmpty()) {
+            query.setParameter("name", "%" + nameFilter + "%");
+        }
+
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+
+        return query.getResultList(); // container manages EM lifecycle
     }
 
+    /**
+     * Count total products optionally filtered by name.
+     */
     public long countProducts(String nameFilter) {
-        EntityManager em = getEntityManager();
-        try {
-            String jpql = "SELECT COUNT(p) FROM Product p";
-            if (nameFilter != null && !nameFilter.isEmpty()) {
-                jpql += " WHERE LOWER(p.name) LIKE LOWER(:name)";
-            }
-
-            TypedQuery<Long> query = em.createQuery(jpql, Long.class);
-
-            if (nameFilter != null && !nameFilter.isEmpty()) {
-                query.setParameter("name", "%" + nameFilter + "%");
-            }
-
-            return query.getSingleResult();
-        } finally {
-            em.close();
+        String jpql = "SELECT COUNT(p) FROM Product p";
+        if (nameFilter != null && !nameFilter.isEmpty()) {
+            jpql += " WHERE LOWER(p.name) LIKE LOWER(:name)";
         }
+
+        TypedQuery<Long> query = getEntityManager().createQuery(jpql, Long.class);
+
+        if (nameFilter != null && !nameFilter.isEmpty()) {
+            query.setParameter("name", "%" + nameFilter + "%");
+        }
+
+        return query.getSingleResult(); // container manages EM lifecycle
     }
 }

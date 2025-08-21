192ea7054b87b68d408b862cab1fb551904c3a23
Morebotsane <morebotsane@gmail.com>
2025-08-21 04:43:15 +0100

    The all new Customer mapper. Currently working on the New and improved CustomerService class.

---
+++ ProductCrudApp/src/main/java/com/example/dto/mappers/CustomerMapper.java
@@ -0,0 +1,34 @@
+package com.example.dto.mappers;
+
+import com.example.dto.CustomerRequest;
+import com.example.dto.CustomerResponse;
+import com.example.entities.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerResponse toResponse(Customer entity) {
+        if (entity == null) return null;
+        return new CustomerResponse(
+                entity.getId(),
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getEmail()
+        );
+    }
+
+    public static Customer toEntity(CustomerRequest request) {
+        if (request == null) return null;
+        Customer customer = new Customer();
+        customer.setFirstName(request.getFirstName());
+        customer.setLastName(request.getLastName());
+        customer.setEmail(request.getEmail());
+        return customer;
+    }
+
+    public static void updateEntity(Customer entity, CustomerRequest request) {
+        if (request == null || entity == null) return;
+        entity.setFirstName(request.getFirstName());
+        entity.setLastName(request.getLastName());
+        entity.setEmail(request.getEmail());
+    }
+}
